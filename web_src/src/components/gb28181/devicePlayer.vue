f4ef375835df1c3507621b2d52edea45729f98e8
Lawrence <1934378145@qq.com>
2020-12-16 12:51:57 +0800

    修正播放窗口关闭时销毁播放器的代码

--- web_src/src/components/gb28181/devicePlayer.vue
+++ web_src/src/components/gb28181/devicePlayer.vue
@@ -306,7 +306,7 @@ export default {
         },
         close: function () {
             console.log('关闭视频');
-            if (this.$refs.videoPlayer){
+            if (!!this.$refs.videoPlayer){
               this.$refs.videoPlayer.pause();
             }
             this.videoUrl = '';

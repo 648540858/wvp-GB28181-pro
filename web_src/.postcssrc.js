67cc6685004cf04fb6323cb4811992ebc783bd3e
Lawrence <1934378145@qq.com>
2021-01-21 20:02:33 +0800

    增加页面字体大小设置，方便增加功能调整页面

--- web_src/.postcssrc.js
+++ web_src/.postcssrc.js
@@ -9,9 +9,6 @@ module.exports = {
     'postcss-pxtorem': {
       rootValue: 24, 
       propList: ['font-size'] // 只转化font-size
-        // propList: ['*'], // 转化全部
-        // propList: ['*','!border'], //转化全部，除了border属性
-        // selectorBlackList: ['body'] // 过滤掉.am-开头的class，不进行rem转换
     }
   }
 }

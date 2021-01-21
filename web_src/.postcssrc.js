0456d6e357fa12b903a486431f64720f18683671
Lawrence <1934378145@qq.com>
2021-01-21 19:51:23 +0800

    支持字体大小调整，方便扩展功能

--- web_src/.postcssrc.js
+++ web_src/.postcssrc.js
@@ -5,6 +5,13 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    'postcss-pxtorem': {
+      rootValue: 24, 
+      propList: ['font-size'] // 只转化font-size
+        // propList: ['*'], // 转化全部
+        // propList: ['*','!border'], //转化全部，除了border属性
+        // selectorBlackList: ['body'] // 过滤掉.am-开头的class，不进行rem转换
+    }
   }
 }

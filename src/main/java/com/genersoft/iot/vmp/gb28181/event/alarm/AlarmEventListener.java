4b9549dfbf55d8af415efe609623e9d19dd6e6d4
Lawrence <1934378145@qq.com>
2021-01-21 19:57:57 +0800

    添加浏览器ID，确保SSE可同时推送到不同的前端

--- src/main/java/com/genersoft/iot/vmp/gb28181/event/alarm/AlarmEventListener.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/event/alarm/AlarmEventListener.java
@@ -4,6 +4,10 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,10 +22,10 @@ public class AlarmEventListener implements ApplicationListener<AlarmEvent> {
 
     private final static Logger logger = LoggerFactory.getLogger(AlarmEventListener.class);
 
-    private static SseEmitter emitter = new SseEmitter();
+    private static Map<String, SseEmitter> sseEmitters = new Hashtable<>();
 
-    public void addSseEmitters(SseEmitter sseEmitter) {
-        emitter = sseEmitter;
+    public void addSseEmitters(String browserId, SseEmitter sseEmitter) {
+        sseEmitters.put(browserId, sseEmitter);
     }
 
     @Override
@@ -30,18 +34,25 @@ public class AlarmEventListener implements ApplicationListener<AlarmEvent> {
             logger.debug("设备报警事件触发，deviceId：" + event.getAlarmInfo().getDeviceId() + ", "
                     + event.getAlarmInfo().getAlarmDescription());
         }
-        try {
-            String msg = "<strong>设备编码：</strong> <i>" + event.getAlarmInfo().getDeviceId() + "</i>"
-                        + "<br><strong>报警描述：</strong> <i>" + event.getAlarmInfo().getAlarmDescription() + "</i>"
-                        + "<br><strong>报警时间：</strong> <i>" + event.getAlarmInfo().getAlarmTime() + "</i>"
-                        + "<br><strong>定位经度：</strong> <i>" + event.getAlarmInfo().getLongitude() + "</i>"
-                        + "<br><strong>定位纬度：</strong> <i>" + event.getAlarmInfo().getLatitude() + "</i>";
-            emitter.send(msg);
-        } catch (IOException e) {
-            if (logger.isDebugEnabled()) {
-                logger.debug("SSE 通道已关闭");
+        String msg = "<strong>设备编码：</strong> <i>" + event.getAlarmInfo().getDeviceId() + "</i>"
+                    + "<br><strong>报警描述：</strong> <i>" + event.getAlarmInfo().getAlarmDescription() + "</i>"
+                    + "<br><strong>报警时间：</strong> <i>" + event.getAlarmInfo().getAlarmTime() + "</i>"
+                    + "<br><strong>报警位置：</strong> <i>" + event.getAlarmInfo().getLongitude() + "</i>"
+                    + ", <i>" + event.getAlarmInfo().getLatitude() + "</i>";
+
+        for (Iterator<Map.Entry<String, SseEmitter>> it = sseEmitters.entrySet().iterator(); it.hasNext();) {
+            Map.Entry<String, SseEmitter> emitter = it.next();
+            logger.info("推送到SSE连接，浏览器ID: " + emitter.getKey());
+            try {
+                emitter.getValue().send(msg);
+            } catch (IOException | IllegalStateException e) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("SSE连接已关闭");
+                }
+                // 移除已关闭的连接
+                it.remove();
+                // e.printStackTrace();
             }
-            // e.printStackTrace();
         }
     }
 }

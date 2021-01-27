70564219ae079cadf95997e3cfb49a1c8277db52
Lawrence <1934378145@qq.com>
2021-01-27 15:54:14 +0800

    增加SSE跨域支持

--- src/main/java/com/genersoft/iot/vmp/vmanager/SseController/SseController.java
+++ src/main/java/com/genersoft/iot/vmp/vmanager/SseController/SseController.java
@@ -3,6 +3,7 @@ package com.genersoft.iot.vmp.vmanager.SseController;
 import com.genersoft.iot.vmp.gb28181.event.alarm.AlarmEventListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
  * @data: 2021-01-20
  */
 
+@CrossOrigin
 @Controller
 @RequestMapping("/api")
 public class SseController {

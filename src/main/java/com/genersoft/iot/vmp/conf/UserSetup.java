a603f9978632fd0a5806ac87389fe8dd254e23b9
64850858 <648540858@qq.com>
2021-07-07 18:03:05 +0800

    修复配置文件赋值失败

--- src/main/java/com/genersoft/iot/vmp/conf/UserSetup.java
+++ src/main/java/com/genersoft/iot/vmp/conf/UserSetup.java
@@ -1,75 +1,10 @@
 package com.genersoft.iot.vmp.conf;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
-//@Configuration("userSetup")
-//public class UserSetup {
-//    @Value("${userSettings.savePositionHistory:false}")
-//    boolean savePositionHistory;
-//
-//    @Value("${userSettings.autoApplyPlay}")
-//    private boolean autoApplyPlay;
-//
-//    @Value("${userSettings.seniorSdp:false}")
-//    private boolean seniorSdp;
-//
-//    @Value("${userSettings.playTimeout:18000}")
-//    private long playTimeout;
-//
-//    @Value("${userSettings.waitTrack:false}")
-//    private boolean waitTrack;
-//
-//    @Value("${userSettings.interfaceAuthentication}")
-//    private boolean interfaceAuthentication;
-//
-//    @Value("${userSettings.recordPushLive}")
-//    private boolean recordPushLive;
-//
-//    @Value("${userSettings.interfaceAuthenticationExcludes:}")
-//    private String interfaceAuthenticationExcludes;
-//
-//    public boolean getSavePositionHistory() {
-//        return savePositionHistory;
-//    }
-//
-//    public boolean isSavePositionHistory() {
-//        return savePositionHistory;
-//    }
-//
-//    public boolean isAutoApplyPlay() {
-//        return autoApplyPlay;
-//    }
-//
-//    public boolean isSeniorSdp() {
-//        return seniorSdp;
-//    }
-//
-//    public long getPlayTimeout() {
-//        return playTimeout;
-//    }
-//
-//    public boolean isWaitTrack() {
-//        return waitTrack;
-//    }
-//
-//    public boolean isInterfaceAuthentication() {
-//        return interfaceAuthentication;
-//    }
-//
-//    public boolean isRecordPushLive() {
-//        return recordPushLive;
-//    }
-//
-//    public String getInterfaceAuthenticationExcludes() {
-//        return interfaceAuthenticationExcludes;
-//    }
-//}
 
 
 @Component
@@ -126,5 +61,37 @@ public class UserSetup {
 
     public List<String> getInterfaceAuthenticationExcludes() {
         return interfaceAuthenticationExcludes;
+    }
+
+    public void setSavePositionHistory(Boolean savePositionHistory) {
+        this.savePositionHistory = savePositionHistory;
+    }
+
+    public void setAutoApplyPlay(Boolean autoApplyPlay) {
+        this.autoApplyPlay = autoApplyPlay;
+    }
+
+    public void setSeniorSdp(Boolean seniorSdp) {
+        this.seniorSdp = seniorSdp;
+    }
+
+    public void setPlayTimeout(Long playTimeout) {
+        this.playTimeout = playTimeout;
+    }
+
+    public void setWaitTrack(Boolean waitTrack) {
+        this.waitTrack = waitTrack;
+    }
+
+    public void setInterfaceAuthentication(boolean interfaceAuthentication) {
+        this.interfaceAuthentication = interfaceAuthentication;
+    }
+
+    public void setRecordPushLive(Boolean recordPushLive) {
+        this.recordPushLive = recordPushLive;
+    }
+
+    public void setInterfaceAuthenticationExcludes(List<String> interfaceAuthenticationExcludes) {
+        this.interfaceAuthenticationExcludes = interfaceAuthenticationExcludes;
     }
 }

1beb220b5bdd242caa1255d319030f3f9d15a167
648540858 <648540858@qq.com>
2022-10-17 17:16:59 +0800

    支持锁定编辑后的设备名 #642

--- sql/update.sql
+++ sql/update.sql
@@ -1,8 +1,8 @@
-alter table wvp.media_server
+alter table media_server
     drop column streamNoneReaderDelayMS;
 
 alter table stream_proxy
     add enable_disable_none_reader bit(1) default null;
 
 alter table device
-    add mediaServerId varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'auto';
+    add custom_name varchar(255) default null;

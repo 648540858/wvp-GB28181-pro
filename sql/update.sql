8228f59cb1a86462fadf2fa238748eec8c6b5d10
韩浩然XiaoQiTong <69035114+XiaoQiTong@users.noreply.github.com>
2022-10-17 17:10:37 +0800

    Update update.sql

--- sql/update.sql
+++ sql/update.sql
@@ -3,3 +3,6 @@ alter table wvp.media_server
 
 alter table stream_proxy
     add enable_disable_none_reader bit(1) default null;
+
+alter table device
+    add mediaServerId varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'auto';

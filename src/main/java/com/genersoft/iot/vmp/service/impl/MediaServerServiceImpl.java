2bf7cdc2df8482b775c05e783e49f3bb7ec58da5
ljm <1271952916@qq.com>
2022-10-20 16:04:47 +0800

    【zlm心跳hook】重复查询redis了 变量也不需要多次定义 数据库有的话应该恢复zlm

--- src/main/java/com/genersoft/iot/vmp/service/impl/MediaServerServiceImpl.java
+++ src/main/java/com/genersoft/iot/vmp/service/impl/MediaServerServiceImpl.java
@@ -644,19 +644,13 @@ public class MediaServerServiceImpl implements IMediaServerService {
         MediaServerItem mediaServerItem = getOne(mediaServerId);
         if (mediaServerItem == null) {
             // 缓存不存在，从数据库查询，如果数据库不存在则是错误的
-            MediaServerItem mediaServerItemFromDatabase = getOneFromDatabase(mediaServerId);
-            if (mediaServerItemFromDatabase == null) {
-                return;
-            }
-            // zlm连接重试
-            logger.warn("[更新ZLM 保活信息]失败，未找到流媒体信息,尝试重连zlm");
-//            reloadZlm();
-            mediaServerItem = getOne(mediaServerId);
+            mediaServerItem = getOneFromDatabase(mediaServerId);
             if (mediaServerItem == null) {
-                // zlm连接重试
                 logger.warn("[更新ZLM 保活信息]失败，未找到流媒体信息");
                 return;
             }
+            // zlm连接重试
+            logger.warn("[更新ZLM 保活信息]尝试链接默认zml");
         }
         final String zlmKeepaliveKey = zlmKeepaliveKeyPrefix + mediaServerItem.getId();
         dynamicTask.stop(zlmKeepaliveKey);

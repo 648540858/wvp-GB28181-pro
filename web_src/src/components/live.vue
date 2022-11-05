aae7c80fd5c6d89fb392e86648920db6c79eca51
xiaoQQya <xiaoQQya@126.com>
2022-11-05 12:50:26 +0800

    fix(live): 修复分屏监控 HTTPS 环境下无法播放的问题

--- web_src/src/components/live.vue
+++ web_src/src/components/live.vue
@@ -138,8 +138,14 @@ export default {
         url: '/api/play/start/' + deviceId + '/' + channelId
       }).then(function (res) {
         if (res.data.code === 0 && res.data.data) {
-          itemData.playUrl = res.data.data.httpsFlv
-          that.setPlayUrl(res.data.data.ws_flv, idxTmp)
+          let videoUrl;
+          if (location.protocol === "https:") {
+            videoUrl = res.data.data.wss_flv;
+          } else {
+            videoUrl = res.data.data.ws_flv;
+          }
+          itemData.playUrl = videoUrl;
+          that.setPlayUrl(videoUrl, idxTmp);
         } else {
           that.$message.error(res.data.msg);
         }

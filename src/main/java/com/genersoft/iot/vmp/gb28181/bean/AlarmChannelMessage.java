40ece192fe8b7c2df8599a71daeda422083c5a47
gaofw189 <gaofw189@chinatelecom.cn>
2023-02-03 14:35:44 +0800

    修复WVP作为下级平台接收设备告警消息后上报上级平台的问题

--- src/main/java/com/genersoft/iot/vmp/gb28181/bean/AlarmChannelMessage.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/bean/AlarmChannelMessage.java
@@ -1,46 +1,28 @@
 package com.genersoft.iot.vmp.gb28181.bean;
 
+import lombok.Data;
+
 /**
  * 通过redis分发报警消息
  */
+@Data
 public class AlarmChannelMessage {
     /**
      * 国标编号
      */
     private String gbId;
-
     /**
      * 报警编号
      */
     private int alarmSn;
-
+    /**
+     * 告警类型
+     */
+    private int alarmType;
 
     /**
      * 报警描述
      */
     private String alarmDescription;
 
-    public String getGbId() {
-        return gbId;
-    }
-
-    public void setGbId(String gbId) {
-        this.gbId = gbId;
-    }
-
-    public int getAlarmSn() {
-        return alarmSn;
-    }
-
-    public void setAlarmSn(int alarmSn) {
-        this.alarmSn = alarmSn;
-    }
-
-    public String getAlarmDescription() {
-        return alarmDescription;
-    }
-
-    public void setAlarmDescription(String alarmDescription) {
-        this.alarmDescription = alarmDescription;
-    }
 }

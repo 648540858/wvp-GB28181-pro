f4e5e0396245f249153f76382772d943acc1192a
648540858 <648540858@qq.com>
2023-06-21 15:53:26 +0800

    修复表名错误 #904

--- src/main/java/com/genersoft/iot/vmp/storager/dao/DeviceChannelMapper.java
+++ src/main/java/com/genersoft/iot/vmp/storager/dao/DeviceChannelMapper.java
@@ -457,7 +457,7 @@ public interface DeviceChannelMapper {
     // 设备主子码流逻辑END
     @Select(value = {" <script>" +
             "select * " +
-            "from device_channel " +
+            "from wvp_device_channel " +
             "where device_id=#{deviceId}" +
             " <if test='parentId != null '> and parent_id = #{parentId} </if>" +
             " <if test='parentId == null '> and parent_id is null </if>" +

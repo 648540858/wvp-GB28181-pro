62753724553a69e31f121f186668dd11562e3ec8
648540858 <648540858@qq.com>
2023-07-31 17:34:44 +0800

    添加service

---
+++ src/main/java/com/genersoft/iot/vmp/jt1078/service/Ijt1078Service.java
@@ -0,0 +1,4 @@
+package com.genersoft.iot.vmp.jt1078.service;
+
+public interface Ijt1078Service {
+}
---
+++ src/main/java/com/genersoft/iot/vmp/jt1078/service/impl/jt1078ServiceImpl.java
@@ -0,0 +1,8 @@
+package com.genersoft.iot.vmp.jt1078.service.impl;
+
+import com.genersoft.iot.vmp.jt1078.service.Ijt1078Service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class jt1078ServiceImpl implements Ijt1078Service {
+}

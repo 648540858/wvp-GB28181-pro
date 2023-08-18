69217c221f8f8c2aa0016efc22f62e4085144957
648540858 <648540858@qq.com>
2023-08-18 12:20:40 +0800

    构建标准国标目录信息

--- src/main/java/com/genersoft/iot/vmp/common/CommonGbChannel.java
+++ src/main/java/com/genersoft/iot/vmp/common/CommonGbChannel.java
@@ -5,44 +5,165 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public class CommonGbChannel {
 
     /**
-     * 国标字段：归属
+     * 国标字段：设备/区域/系统编码(必选)
      */
-    @Schema(description = "归属")
-    private String owner;
+    @Schema(description = "设备/区域/系统编码(必选)")
+    private String commonGbDeviceID;
 
     /**
-     * 国标字段：行政区划
+     * 国标字段：设备/区域/系统名称(必选)
      */
-    @Schema(description = "行政区划")
-    private String civilCode;
+    @Schema(description = "设备/区域/系统名称(必选)")
+    private String commonGbName;
 
     /**
-     * 国标字段：安装地址
+     * 国标字段：当为设备时,设备厂商(必选)
      */
-    @Schema(description = "安装地址")
-    private String address;
+    @Schema(description = "当为设备时,设备厂商(必选)")
+    private String commonGbManufacturer;
 
     /**
-     * 国标字段：经度
+     * 国标字段：当为设备时,设备型号(必选)
      */
-    @Schema(description = "经度")
-    private Double longitude;
+    @Schema(description = "当为设备时,设备型号(必选)")
+    private String commonGbModel;
 
     /**
-     * 国标字段：纬度
+     * 国标字段：当为设备时,设备归属(必选)
      */
-    @Schema(description = "纬度")
-    private Double latitude;
+    @Schema(description = "当为设备时,设备归属(必选)")
+    private String commonGbOwner;
 
     /**
-     * 国标字段：摄像机类型:
+     * 国标字段：行政区域(必选)
+     */
+    @Schema(description = "行政区域(必选)")
+    private String commonGbCivilCode;
+
+    /**
+     * 国标字段：警区(可选)
+     */
+    @Schema(description = "警区(可选)")
+    private String commonGbBlock;
+
+    /**
+     * 国标字段：当为设备时,安装地址(必选)
+     */
+    @Schema(description = "当为设备时,安装地址(必选)")
+    private String commonGbAddress;
+
+    /**
+     * 国标字段：当为设备时,是否有子设备(必选)1有,0没有
+     */
+    @Schema(description = "当为设备时,是否有子设备(必选)1有,0没有")
+    private Integer commonGbParental;
+
+    /**
+     * 国标字段：父设备/区域/系统ID(必选)
+     */
+    @Schema(description = "父设备/区域/系统ID(必选)")
+    private String commonGbParentID;
+
+    /**
+     * 国标字段：信令安全模式(可选)缺省为0; 0:不采用;2:S/MIME 签名方式;3:S/
+     * MIME加密签名同时采用方式;4:数字摘要方式
+     */
+    @Schema(description = "信令安全模式(可选)缺省为0; " +
+            "0:不采用;" +
+            "2:S/MIME 签名方式;" +
+            "3:S/MIME加密签名同时采用方式;4:数字摘要方式")
+    private Integer commonGbSafetyWay;
+
+    /**
+     * 国标字段：注册方式(必选)缺省为1;1:符合IETFRFC3261标准的认证注册模
+     * 式;2:基于口令的双向认证注册模式;3:基于数字证书的双向认证注册模式
+     */
+    @Schema(description = "注册方式(必选)缺省为1;" +
+            "1:符合IETFRFC3261标准的认证注册模式;" +
+            "2:基于口令的双向认证注册模式;" +
+            "3:基于数字证书的双向认证注册模式")
+    private Integer commonGbRegisterWay;
+
+    /**
+     * 证书序列号(有证书的设备必选)
+     */
+    @Schema(description = "证书序列号(有证书的设备必选)")
+    private String commonGbCertNum;
+
+    /**
+     * 证书有效标识(有证书的设备必选)缺省为0;证书有效标识:0:无效 1:有效
+     */
+    @Schema(description = "证书有效标识(有证书的设备必选)缺省为0;证书有效标识:0:无效 1:有效")
+    private Integer commonGbCertifiable;
+
+    /**
+     * 无效原因码(有证书且证书无效的设备必选)
+     */
+    @Schema(description = "无效原因码(有证书且证书无效的设备必选)")
+    private Integer commonGbErrCode;
+
+    /**
+     * 证书终止有效期(有证书的设备必选)
+     */
+    @Schema(description = "证书终止有效期(有证书的设备必选)")
+    private String commonGbEndTime;
+
+    /**
+     * 保密属性(必选)缺省为0;0:不涉密,1:涉密
+     */
+    @Schema(description = "保密属性(必选)缺省为0;0:不涉密,1:涉密")
+    private Integer commonGbSecrecy;
+
+    /**
+     * 设备/区域/系统IP地址(可选)
+     */
+    @Schema(description = "设备/区域/系统IP地址(可选)")
+    private String commonIPAddress;
+
+    /**
+     * 设备/区域/系统端口(可选)
+     */
+    @Schema(description = "设备/区域/系统端口(可选)")
+    private String commonPort;
+
+    /**
+     * 设备口令(可选)
+     */
+    @Schema(description = "设备口令(可选)")
+    private String commonPassword;
+
+
+    /**
+     * 设备状态(必选)
+     */
+    @Schema(description = "设备状态(必选)")
+    private String commonStatus;
+
+    /**
+     * 国标字段：经度(可选)
+     */
+    @Schema(description = "经度(可选)")
+    private Double commonGbLongitude;
+
+    /**
+     * 国标字段：纬度(可选)
+     */
+    @Schema(description = "纬度(可选)")
+    private Double commonGbLatitude;
+
+    /**
+     * 国标字段：摄像机类型扩展,标识摄像机类型, 当目录项为摄像机时可选:
      * 1-球机;
      * 2-半球;
      * 3-固定枪机;
      * 4-遥控枪机
      */
-    @Schema(description = "摄像机类型")
-    private Integer ptzType;
+    @Schema(description = "摄像机类型扩展,标识摄像机类型,当目录项为摄像机时可选:\n" +
+            "      1-球机;\n" +
+            "      2-半球;\n" +
+            "      3-固定枪机;\n" +
+            "      4-遥控枪机")
+    private Integer commonGbPtzType;
 
     /**
      * 国标字段：摄像机位置类型扩展。
@@ -57,25 +178,38 @@ public class CommonGbChannel {
      * 9-治安复杂区域、
      * 10-交通干线
      */
-    @Schema(description = "摄像机位置类型扩展")
-    private Integer positionType;
+    @Schema(description = "摄像机位置类型扩展" +
+            "      1-省际检查站、\n" +
+            "      2-党政机关、\n" +
+            "      3-车站码头、\n" +
+            "      4-中心广场、\n" +
+            "      5-体育场馆、\n" +
+            "      6-商业中心、\n" +
+            "      7-宗教场所、\n" +
+            "      8-校园周边、\n" +
+            "      9-治安复杂区域、\n" +
+            "      10-交通干线")
+    private Integer commonGbPositionType;
 
     /**
-     * 国标字段：安装位置室外、室内属性
+     * 国标字段：安装位置室外、室内属性, 当目录项为摄像机时可选。
      * 1-室外、
      * 2-室内
      */
-    @Schema(description = "安装位置室外、室内属性")
-    private Integer roomType;
+    @Schema(description = "安装位置室外、室内属性, 1-室外、2-室内")
+    private Integer commonGbRoomType;
 
     /**
-     * 国标字段：用途
+     * 国标字段：用途, 当目录项为摄像机时可选。
      * 1-治安、
      * 2-交通、
      * 3-重点、
      */
-    @Schema(description = "用途")
-    private Integer useType;
+    @Schema(description = "用途, " +
+            "      1-治安、\n" +
+            "      2-交通、\n" +
+            "      3-重点、")
+    private Integer commonGbUseType;
 
     /**
      * 国标字段：补光属性
@@ -83,11 +217,14 @@ public class CommonGbChannel {
      * 2-红外补光、
      * 3-白光补光
      */
-    @Schema(description = "补光属性")
-    private Integer supplyLightType;
+    @Schema(description = "补光属性,      " +
+            "      1-无补光、\n" +
+            "      2-红外补光、\n" +
+            "      3-白光补光")
+    private Integer commonGbSupplyLightType;
 
     /**
-     * 摄像机监视方位属性。
+     * 摄像机监视方位属性。当目录项为摄像机时且为固定摄像机或设置看守位摄像机时可选。
      * 1-东、
      * 2-西、
      * 3-南、
@@ -98,94 +235,33 @@ public class CommonGbChannel {
      * 8-西北
      *
      */
-    @Schema(description = "方位")
-    private Integer directionType;
+    @Schema(description = "方位, 当目录项为摄像机时且为固定摄像机或设置看守位摄像机时可选。" +
+            "      1-东、\n" +
+            "      2-西、\n" +
+            "      3-南、\n" +
+            "      4-北、\n" +
+            "      5-东南、\n" +
+            "      6-东北、\n" +
+            "      7-西南、\n" +
+            "      8-西北")
+    private Integer commonGbDirectionType;
 
-    public String getOwner() {
-        return owner;
-    }
+    /**
+     * 摄像机支持的分辨率,可有多个分辨率值,各个取值间以“/”分隔。
+     * 分辨率取值参见附录 F中SDPf字段规定。当目录项为摄像机时可选。
+     */
+    @Schema(description = "摄像机支持的分辨率,可有多个分辨率值,各个取值间以“/”分隔。" +
+            "分辨率取值参见附录 F中SDPf字段规定。当目录项为摄像机时可选。")
+    private Integer commonGbResolution;
 
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+    /**
+     * 摄像机支持的分辨率,可有多个分辨率值,各个取值间以“/”分隔。
+     * 分辨率取值参见附录 F中SDPf字段规定。当目录项为摄像机时可选。
+     *
+     */
+    @Schema(description = "摄像机支持的分辨率,可有多个分辨率值,各个取值间以“/”分隔。" +
+            "分辨率取值参见附录 F中SDPf字段规定。当目录项为摄像机时可选。")
+    private Integer commonGbBusinessGroupID;
 
-    public String getCivilCode() {
-        return civilCode;
-    }
 
-    public void setCivilCode(String civilCode) {
-        this.civilCode = civilCode;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Integer getPtzType() {
-        return ptzType;
-    }
-
-    public void setPtzType(Integer ptzType) {
-        this.ptzType = ptzType;
-    }
-
-    public Integer getPositionType() {
-        return positionType;
-    }
-
-    public void setPositionType(Integer positionType) {
-        this.positionType = positionType;
-    }
-
-    public Integer getRoomType() {
-        return roomType;
-    }
-
-    public void setRoomType(Integer roomType) {
-        this.roomType = roomType;
-    }
-
-    public Integer getUseType() {
-        return useType;
-    }
-
-    public void setUseType(Integer useType) {
-        this.useType = useType;
-    }
-
-    public Integer getSupplyLightType() {
-        return supplyLightType;
-    }
-
-    public void setSupplyLightType(Integer supplyLightType) {
-        this.supplyLightType = supplyLightType;
-    }
-
-    public Integer getDirectionType() {
-        return directionType;
-    }
-
-    public void setDirectionType(Integer directionType) {
-        this.directionType = directionType;
-    }
 }

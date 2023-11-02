073b6acec373654944815dd5dbd4303e3cb02a03
648540858 <648540858@qq.com>
2023-11-02 15:25:39 +0800

    取出多余注释

--- src/main/java/com/genersoft/iot/vmp/service/impl/MediaServerServiceImpl.java
+++ src/main/java/com/genersoft/iot/vmp/service/impl/MediaServerServiceImpl.java
@@ -577,11 +577,6 @@ public class MediaServerServiceImpl implements IMediaServerService {
         param.put("hook.on_send_rtp_stopped",String.format("%s/on_send_rtp_stopped", hookPrex));
         param.put("hook.on_rtp_server_timeout",String.format("%s/on_rtp_server_timeout", hookPrex));
         param.put("hook.on_record_mp4",String.format("%s/on_record_mp4", hookPrex));
-//        if (mediaServerItem.getRecordAssistPort() > 0) {
-//            param.put("hook.on_record_mp4",String.format("http://127.0.0.1:%s/api/record/on_record_mp4", mediaServerItem.getRecordAssistPort()));
-//        }else {
-//            param.put("hook.on_record_mp4","");
-//        }
         param.put("hook.timeoutSec","20");
         // 推流断开后可以在超时时间内重新连接上继续推流，这样播放器会接着播放。
         // 置0关闭此特性(推流断开会导致立即断开播放器)
@@ -590,7 +585,6 @@ public class MediaServerServiceImpl implements IMediaServerService {
         param.put("protocol.continue_push_ms", "3000" );
         // 最多等待未初始化的Track时间，单位毫秒，超时之后会忽略未初始化的Track, 设置此选项优化那些音频错误的不规范流，
         // 等zlm支持给每个rtpServer设置关闭音频的时候可以不设置此选项
-//        param.put("general.wait_track_ready_ms", "3000" );
         if (mediaServerItem.isRtpEnable() && !ObjectUtils.isEmpty(mediaServerItem.getRtpPortRange())) {
             param.put("rtp_proxy.port_range", mediaServerItem.getRtpPortRange().replace(",", "-"));
         }

25a46d183354bd63426d92670afdd89f8ca8c864
648540858 <648540858@qq.com>
2024-01-14 11:15:46 +0800

    注释优化

--- src/main/java/com/genersoft/iot/vmp/service/IStreamProxyService.java
+++ src/main/java/com/genersoft/iot/vmp/service/IStreamProxyService.java
@@ -15,33 +15,23 @@ public interface IStreamProxyService {
 
     /**
      * 保存视频代理
-     * @param param
      */
     void save(StreamProxy param, GeneralCallback<StreamInfo> callback);
 
 
     /**
      * 分页查询
-     * @param page
-     * @param count
-     * @return
      */
     PageInfo<StreamProxy> getAll(String query, Boolean online, String mediaServerId, Integer page, Integer count);
 
 
     /**
      * 启用视频代理
-     * @param app
-     * @param stream
-     * @return
      */
     void start(String app, String stream, GeneralCallback<StreamInfo> callback);
 
     /**
      * 更新状态
-     * @param status 状态
-     * @param app
-     * @param stream
      */
     void updateStatus(boolean status, String app, String stream);
 
@@ -49,42 +39,32 @@ public interface IStreamProxyService {
 
     /**
      * 停用用视频代理
-     * @param app
-     * @param stream
-     * @return
      */
     void stop(String app, String stream, GeneralCallback<StreamInfo> callback);
 
     /**
      * 获取ffmpeg.cmd模板
-     * @return
      */
     JSONObject getFFmpegCMDs(MediaServerItem mediaServerItem);
 
     /**
      * 根据app与stream获取streamProxy
-     * @return
      */
     StreamProxy getStreamProxyByAppAndStream(String app, String streamId);
 
 
     /**
      * 新的节点加入
-     * @param mediaServerId
-     * @return
      */
     void zlmServerOnline(String mediaServerId);
 
     /**
      * 节点离线
-     * @param mediaServerId
-     * @return
      */
     void zlmServerOffline(String mediaServerId);
 
     /**
      * 获取统计信息
-     * @return
      */
     ResourceBaseInfo getOverview();
 
@@ -103,7 +83,10 @@ public interface IStreamProxyService {
      */
     void add(StreamProxy param, GeneralCallback<StreamInfo> callback);
 
-    void removeProxy(int gbId);
+    /**
+     *
+     */
+    void removeProxy(int id);
 
     /**
      * 编辑拉流代理

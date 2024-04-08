9badf1c7fe71c081ff418ae43c1b950d4db2e584
648540858 <648540858@qq.com>
2024-04-08 18:15:02 +0800

    1078-添加位置附加信息定义

--- src/main/java/com/genersoft/iot/vmp/jt1078/bean/JTPositionInfo.java
+++ src/main/java/com/genersoft/iot/vmp/jt1078/bean/JTPositionInfo.java
@@ -6,142 +6,30 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public class JTPositionInfo {
 
     /**
-     * 报警标志
+     * 位置基本信息
      */
-    @Schema(description = "报警标志")
-    private JTAlarmSign alarmSign;
+    @Schema(description = "位置基本信息")
+    private JTPositionBaseInfo base;
 
     /**
-     * 状态
+     * 位置基本信息
      */
-    @Schema(description = "状态")
-    private JTStatus status;
+    @Schema(description = "位置附加信息")
+    private JTPositionAdditionalInfo additional;
 
-    /**
-     * 经度
-     */
-    @Schema(description = "经度")
-    private Double longitude;
-
-    /**
-     * 纬度
-     */
-    @Schema(description = "纬度")
-    private Double latitude;
-
-    /**
-     * 高程
-     */
-    @Schema(description = "高程")
-    private Integer altitude;
-
-    /**
-     * 速度
-     */
-    @Schema(description = "速度")
-    private Integer speed;
-
-    /**
-     * 方向
-     */
-    @Schema(description = "方向")
-    private Integer direction;
-
-    /**
-     * 时间
-     */
-    @Schema(description = "时间")
-    private String time;
-
-    /**
-     * 视频报警
-     */
-    @Schema(description = "视频报警")
-    private JTVideoAlarm videoAlarm;
-
-    public JTAlarmSign getAlarmSign() {
-        return alarmSign;
+    public JTPositionBaseInfo getBase() {
+        return base;
     }
 
-    public void setAlarmSign(JTAlarmSign alarmSign) {
-        this.alarmSign = alarmSign;
+    public void setBase(JTPositionBaseInfo base) {
+        this.base = base;
     }
 
-    public JTStatus getStatus() {
-        return status;
+    public JTPositionAdditionalInfo getAdditional() {
+        return additional;
     }
 
-    public void setStatus(JTStatus status) {
-        this.status = status;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Integer getAltitude() {
-        return altitude;
-    }
-
-    public void setAltitude(Integer altitude) {
-        this.altitude = altitude;
-    }
-
-    public Integer getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(Integer speed) {
-        this.speed = speed;
-    }
-
-    public Integer getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Integer direction) {
-        this.direction = direction;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public JTVideoAlarm getVideoAlarm() {
-        return videoAlarm;
-    }
-
-    public void setVideoAlarm(JTVideoAlarm videoAlarm) {
-        this.videoAlarm = videoAlarm;
-    }
-
-    @Override
-    public String toString() {
-        return "位置汇报信息： " +
-                " \n 报警标志：" + alarmSign.toString() +
-                " \n 状态：" + status.toString() +
-                " \n 经度：" + longitude +
-                " \n 纬度：" + latitude +
-                " \n 高程： " + altitude +
-                " \n 速度： " + speed +
-                " \n 方向： " + direction +
-                " \n 时间： " + time +
-                " \n";
+    public void setAdditional(JTPositionAdditionalInfo additional) {
+        this.additional = additional;
     }
 }

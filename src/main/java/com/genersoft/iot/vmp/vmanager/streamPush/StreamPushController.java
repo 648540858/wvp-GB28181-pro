f6157cc42c048a15f9962653cc957e4a32849bb2
Iwan Lebron <105045937+iwanlebron@users.noreply.github.com>
2024-06-10 09:18:37 +0800

    修复参数名称

--- src/main/java/com/genersoft/iot/vmp/vmanager/streamPush/StreamPushController.java
+++ src/main/java/com/genersoft/iot/vmp/vmanager/streamPush/StreamPushController.java
@@ -112,7 +112,7 @@ public class StreamPushController {
     @ResponseBody
     @Operation(summary = "中止一个推流", security = @SecurityRequirement(name = JwtUtils.HEADER))
     @Parameter(name = "app", description = "应用名", required = true)
-    @Parameter(name = "stream", description = "流id", required = true)
+    @Parameter(name = "streamId", description = "流id", required = true)
     public void stop(String app, String streamId){
         if (!streamPushService.stop(app, streamId)){
             throw new ControllerException(ErrorCode.ERROR100);

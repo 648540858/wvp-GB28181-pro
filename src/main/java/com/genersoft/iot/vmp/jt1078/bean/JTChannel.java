4ba04661ab3d4d366fba2c2f41a1dd3ab204457c
panlinlin <648540858@qq.com>
2024-06-16 09:14:35 +0800

    1078-添加接入国标通道表

--- src/main/java/com/genersoft/iot/vmp/jt1078/bean/JTChannel.java
+++ src/main/java/com/genersoft/iot/vmp/jt1078/bean/JTChannel.java
@@ -44,6 +44,145 @@ public class JTChannel {
     @Schema(description = "流信息")
     private String stream;
 
+    // 国标28181信息
+    @Schema(description = "国标-编码")
+    private String gbDeviceId;
+
+    @Schema(description = "国标-名称")
+    private String gbName;
+
+    @Schema(description = "国标-设备厂商")
+    private String gbManufacturer;
+
+    @Schema(description = "国标-设备型号")
+    private String gbModel;
+
+    @Schema(description = "国标-行政区域")
+    private String gbCivilCode;
+
+    @Schema(description = "国标-警区")
+    private String gbBlock;
+
+    @Schema(description = "国标-安装地址")
+    private String gbAddress;
+
+    @Schema(description = "国标-是否有子设备")
+    private Boolean gbParental;
+
+    @Schema(description = "国标-父节点ID")
+    private String gbParentId;
+
+    @Schema(description = "国标-注册方式")
+    private Integer gbRegisterWay;
+
+    @Schema(description = "国标-编码")
+    private String gbSecurityLevelCode;
+
+    @Schema(description = "国标-编码")
+    private String gbSecrecy;
+
+    @Schema(description = "国标-编码")
+    private String gbIpAddress;
+
+    @Schema(description = "国标-编码")
+    private String gbPort;
+
+    @Schema(description = "国标-编码")
+    private String gbPassword;
+
+    @Schema(description = "国标-编码")
+    private String gbStatus;
+
+    @Schema(description = "国标-编码")
+    private String gbLongitude;
+
+    @Schema(description = "国标-编码")
+    private String gbLatitude;
+
+    @Schema(description = "国标-编码")
+    private String gbBusinessGroupId;
+
+    @Schema(description = "国标-编码")
+    private String gbPtzType;
+
+    @Schema(description = "国标-编码")
+    private String gbPhotoelectricImagingTyp;
+
+    @Schema(description = "国标-编码")
+    private String gbCapturePositionType;
+
+    @Schema(description = "国标-编码")
+    private String gbRoomType;
+
+    @Schema(description = "国标-编码")
+    private String gbSupplyLightType;
+
+    @Schema(description = "国标-编码")
+    private String gbDirectionType;
+
+    @Schema(description = "国标-编码")
+    private String gbResolution;
+
+    @Schema(description = "国标-编码")
+    private String gbStreamNumberList;
+
+    @Schema(description = "国标-编码")
+    private String gbDownloadSpeed;
+
+    @Schema(description = "国标-编码")
+    private String gbSvcSpaceSupportMod;
+
+    @Schema(description = "国标-编码")
+    private String gbSvcTimeSupportMode;
+
+    @Schema(description = "国标-编码")
+    private String gbSsvcRatioSupportList;
+
+    @Schema(description = "国标-编码")
+    private String gbMobileDeviceType;
+
+    @Schema(description = "国标-编码")
+    private String gbHorizontalFieldAngle;
+
+    @Schema(description = "国标-编码")
+    private String gbVerticalFieldAngle;
+
+    @Schema(description = "国标-编码")
+    private String gbMaxViewDistance;
+
+    @Schema(description = "国标-编码")
+    private String gbGrassrootsCode;
+
+    @Schema(description = "国标-编码")
+    private String gbPoType;
+
+    @Schema(description = "国标-编码")
+    private String gbPoCommonName;
+
+    @Schema(description = "国标-编码")
+    private String gbMac;
+
+    @Schema(description = "国标-编码")
+    private String gbFunctionType;
+
+    @Schema(description = "国标-编码")
+    private String gbEncodeType;
+
+    @Schema(description = "国标-编码")
+    private String gbInstallTime;
+
+    @Schema(description = "国标-编码")
+    private String gbManagementUnit;
+
+    @Schema(description = "国标-编码")
+    private String gbContactInfo;
+
+    @Schema(description = "国标-编码")
+    private String gbRecordSaveDays;
+
+    @Schema(description = "国标-编码")
+    private String gbIndustrialClassification;
+
     public int getId() {
         return id;
     }

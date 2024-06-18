a94b18bde9c16e3ad098dc0944c2624e49233582
panlinlin <648540858@qq.com>
2024-06-18 20:42:09 +0800

    1078-添加接入国标通道页面以及属性

--- web_src/src/components/JTChannelGBParams.vue
+++ web_src/src/components/JTChannelGBParams.vue
@@ -50,81 +50,126 @@
             <el-input v-model="form.gbSecurityLevelCode" clearable></el-input>
           </el-form-item>
           <el-form-item label="保密属性" prop="gbSecrecy">
-            <el-input v-model="form.保密属性" clearable></el-input>
             <el-select v-model="form.gbSecrecy" style="float: left; width: 100%" >
               <el-option label="不涉密" :value="0"></el-option>
               <el-option label="涉密" :value="1"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="无线通信拨号用户名(备)" prop="dialingUsernameBackup">
-            <el-input v-model="form.dialingUsernameBackup" clearable></el-input>
+          <el-form-item label="IP" prop="gbIpAddress">
+            <el-input v-model="form.gbIpAddress" clearable></el-input>
           </el-form-item>
-          <el-form-item label="无线通信拨号密码(备)" prop="dialingPasswordBackup">
-            <el-input v-model="form.dialingPasswordBackup" clearable></el-input>
+          <el-form-item label="端口" prop="gbPort">
+            <el-input v-model="form.gbPort" clearable></el-input>
           </el-form-item>
-          <el-form-item label="IP或域名(备)" prop="addressBackup">
-            <el-input v-model="form.addressBackup" clearable></el-input>
+          <el-form-item label="口令" prop="gbPort">
+            <el-input v-model="form.gbPort" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="gbStatus">
+            <el-switch
+              v-model="form.gbStatus"
+              active-text="在线"
+              inactive-text="离线">
+            </el-switch>
+          </el-form-item>
+          <el-form-item label="经度（WGS-84坐标系）" prop="gbLongitude">
+            <el-input v-model="form.gbLongitude" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="纬度（WGS-84坐标系）" prop="gbLatitude">
+            <el-input v-model="form.gbLatitude" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="虚拟组织所属的业务分组ID" prop="gbBusinessGroupId">
+            <el-input v-model="form.gbBusinessGroupId" clearable></el-input>
           </el-form-item>
 
-          <el-form-item label="APN(从)" prop="apnBackup">
-            <el-input v-model="form.apnBackup" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="无线通信拨号用户名(从)" prop="dialingUsernameSlave">
-            <el-input v-model="form.dialingUsernameSlave" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="无线通信拨号密码(从)" prop="dialingPasswordSlave">
-            <el-input v-model="form.dialingPasswordSlave" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="IP或域名(从)" prop="addressSlave">
-            <el-input v-model="form.addressSlave" clearable></el-input>
-          </el-form-item>
-
-          <el-form-item label="IC卡认证服务器IP(主)" prop="addressIcMaster">
-            <el-input v-model="form.addressIcMaster" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="IC卡认证服务器IP(备)" prop="addressIcMaster">
-            <el-input v-model="form.addressIcBackup" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="IC卡认证服务器TCP端口" prop="tcpPortIcMaster">
-            <el-input v-model="form.tcpPortIcMaster" clearable></el-input>
-          </el-form-item>
-          <el-form-item label="IC卡认证服务器UDP端口" prop="udpPortIcMaster">
-            <el-input v-model="form.udpPortIcMaster" clearable></el-input>
-          </el-form-item>
-        </el-form>
-
-        <el-divider content-position="center">位置汇报</el-divider>
-        <el-form size="mini"  ref="form" :rules="rules" :model="form" label-width="240px" style="display: grid; grid-template-columns: 1fr 1fr 1fr 1fr;">
-          <el-form-item label="策略" prop="locationReportingStrategy">
-            <el-select v-model="form.locationReportingStrategy" style="float: left; width: 100%" >
-              <el-option label="定时汇报" :value="0">定时汇报</el-option>
-              <el-option label="定距汇报" :value="1"></el-option>
-              <el-option label="定时和定距汇报" :value="2"></el-option>
+          <el-form-item label="摄像机结构类型" prop="gbPtzType">
+            <el-select v-model="form.gbPtzType" style="float: left; width: 100%" >
+              <el-option label="球机" :value="1"></el-option>
+              <el-option label="半球" :value="2"></el-option>
+              <el-option label="固定枪机" :value="3"></el-option>
+              <el-option label="遥控枪机" :value="4"></el-option>
+              <el-option label="遥控半球" :value="5"></el-option>
+              <el-option label="多目设备的全景/拼接通道" :value="6"></el-option>
+              <el-option label="多目设备的分割通道" :value="7"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="方案" prop="locationReportingPlan">
-            <el-select v-model="form.locationReportingPlan" style="float: left; width: 100%" >
-              <el-option label="根据ACC状态" :value="0"></el-option>
-              <el-option label="登录状态和ACC状态" :value="1"></el-option>
+
+          <el-form-item label="摄像机光电成像类型" prop="gbPtzType">
+            <el-select multiple v-model="form.gbPtzType" style="float: left; width: 100%" >
+              <el-option label="可见光成像" :value="1"></el-option>
+              <el-option label="热成像" :value="2"></el-option>
+              <el-option label="雷达成像" :value="3"></el-option>
+              <el-option label="X光成像" :value="4"></el-option>
+              <el-option label="深度光场成像" :value="5"></el-option>
+              <el-option label="其他" :value="6"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="驾驶员未登录汇报时间间隔(秒)" prop="reportingIntervalOffline">
-            <el-input v-model="form.reportingIntervalOffline" clearable></el-input>
+          <el-form-item label="摄像机采集部位类型" prop="gbCapturePositionType">
+            <el-input v-model="form.gbCapturePositionType" clearable></el-input>
           </el-form-item>
-          <el-form-item label="休眠时汇报时间间隔(秒)" prop="reportingIntervalDormancy">
-            <el-input v-model="form.reportingIntervalDormancy" clearable></el-input>
+          <el-form-item label="室外/室内" prop="gbRoomType">
+            <el-select multiple v-model="form.gbRoomType" style="float: left; width: 100%" >
+              <el-option label="室外" :value="1"></el-option>
+              <el-option label="室内" :value="2"></el-option>
+            </el-select>
           </el-form-item>
-          <el-form-item label="紧急报警时汇报时间间隔(秒)" prop="reportingIntervalEmergencyAlarm">
-            <el-input v-model="form.reportingIntervalEmergencyAlarm" clearable></el-input>
+          <el-form-item label="摄像机补光属性" prop="gbSupplyLightType">
+            <el-select multiple v-model="form.gbSupplyLightType" style="float: left; width: 100%" >
+              <el-option label="无补光" :value="1"></el-option>
+              <el-option label="红外补光" :value="2"></el-option>
+              <el-option label="白光补光" :value="3"></el-option>
+              <el-option label="激光补光" :value="4"></el-option>
+              <el-option label="其他" :value="9"></el-option>
+            </el-select>
           </el-form-item>
-          <el-form-item label="缺省时间汇报间隔(秒)" prop="reportingIntervalDefault">
-            <el-input v-model="form.reportingIntervalDefault" clearable></el-input>
+          <el-form-item label="摄像机监视方位" prop="udpPortIcMaster">
+            <el-select multiple v-model="form.gbRoomType" style="float: left; width: 100%" >
+              <el-option label="东(西向东)" :value="1"></el-option>
+              <el-option label="西(东向西)" :value="2"></el-option>
+              <el-option label="南(北向南)" :value="3"></el-option>
+              <el-option label="北(南向北)" :value="4"></el-option>
+              <el-option label="东南(西北到东南)" :value="5"></el-option>
+              <el-option label="东北(西南到东北)" :value="6"></el-option>
+              <el-option label="西南(东北到西南)" :value="7"></el-option>
+              <el-option label="西北(东南到西北)" :value="8"></el-option>
+            </el-select>
           </el-form-item>
-          <el-form-item label="缺省距离汇报间隔(米)" prop="reportingDistanceDefault">
-            <el-input v-model="form.reportingDistanceDefault" clearable></el-input>
+          <el-form-item label="摄像机支持的分辨率" prop="gbResolution">
+            <el-input v-model="form.gbResolution" clearable></el-input>
           </el-form-item>
-          <el-form-item label="驾驶员未登录汇报距离间隔(米)" prop="reportingDistanceOffline">
-            <el-input v-model="form.reportingDistanceOffline" clearable></el-input>
+          <el-form-item label="摄像机支持的码流编号列表" prop="gbStreamNumberList">
+            <el-input v-model="form.gbStreamNumberList" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="下载倍速" prop="gbDownloadSpeed">
+            <el-input v-model="form.gbDownloadSpeed" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="空域编码能力" prop="gbSvcSpaceSupportMod">
+            <el-select multiple v-model="form.gbSvcSpaceSupportMod" style="float: left; width: 100%" >
+              <el-option label="不支持" :value="0"></el-option>
+              <el-option label="1级增强(1个增强层)" :value="1"></el-option>
+              <el-option label="2级增强(2个增强层)" :value="2"></el-option>
+              <el-option label="3级增强(3个增强层)" :value="3"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="时域编码能力" prop="gbSvcTimeSupportMode">
+            <el-select multiple v-model="form.gbSvcTimeSupportMode" style="float: left; width: 100%" >
+              <el-option label="不支持" :value="0"></el-option>
+              <el-option label="1级增强" :value="1"></el-option>
+              <el-option label="2级增强" :value="2"></el-option>
+              <el-option label="3级增强" :value="3"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="SSVC增强层与基本层比例能力" prop="gbSsvcRatioSupportList">
+            <el-input v-model="form.gbSsvcRatioSupportList" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="移动采集设备类型" prop="gbMobileDeviceType">
+            <el-select multiple v-model="form.gbMobileDeviceType" style="float: left; width: 100%" >
+              <el-option label="移动机器人载摄像机" :value="1"></el-option>
+              <el-option label="执法记录仪" :value="2"></el-option>
+              <el-option label="移动单兵设备" :value="3"></el-option>
+              <el-option label="车载视频记录设备" :value="4"></el-option>
+              <el-option label="无人机载摄像机" :value="5"></el-option>
+              <el-option label="其他" :value="9"></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item label="休眠时汇报距离间隔(米)" prop="reportingDistanceDormancy">
             <el-input v-model="form.reportingDistanceDormancy" clearable></el-input>

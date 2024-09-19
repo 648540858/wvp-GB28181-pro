1b33afc0ea73a04326ebdf2026fd018c00e22b7e
648540858 <648540858@qq.com>
2024-09-19 11:09:37 +0800

    修复通道列表音频开关

--- src/main/java/com/genersoft/iot/vmp/gb28181/bean/DeviceChannel.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/bean/DeviceChannel.java
@@ -179,7 +179,7 @@ public class DeviceChannel extends CommonGBChannel {
 	private String  streamId;
 
 	@Schema(description = "是否含有音频")
-	private boolean ld;
+	private boolean hasAudio;
 
 	@Schema(description = "GPS的更新时间")
 	private String gpsTime;

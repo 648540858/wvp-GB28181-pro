d69b5d810b21b8c59206ce8db549294b9464bba0
648540858 <648540858@qq.com>
2024-12-11 18:26:18 +0800

    [redis下的多wvp] 优化服务间方法调用

--- src/main/java/com/genersoft/iot/vmp/gb28181/service/IGbChannelRpcPlayService.java
+++
@@ -1,8 +0,0 @@
-package com.genersoft.iot.vmp.gb28181.service;
-
-import com.genersoft.iot.vmp.common.StreamInfo;
-import com.genersoft.iot.vmp.service.bean.ErrorCallback;
-
-public interface IGbChannelRpcPlayService {
-    void play(String serverId, Integer channelId, ErrorCallback<StreamInfo> callback);
-}

73be39127b7ea0424ccd1d41ff5e94cd5ce4beac
南宫茜 <auroracloud4096@gmail.com>
2024-12-27 16:30:44 +0800

    修复数据库内容

--- 数据库/2.7.3/更新-postgresql-kingbase-2.7.3.sql
+++ 数据库/2.7.3/更新-postgresql-kingbase-2.7.3.sql
@@ -22,7 +22,7 @@ update wvp_device_channel wdc
 set data_type = 2, data_device_id = (SELECT stream_push_id from wvp_device_channel where stream_push_id is not null and id = wdc.id )  where stream_push_id is not null;
 
 update wvp_device_channel wdc
-set data_type = 1, data_device_id = (SELECT stream_proxy_id from wvp_device_channel where stream_proxy_id is not null and id = wdc.id )  where stream_proxy_id is not null;
+set data_type = 3, data_device_id = (SELECT stream_proxy_id from wvp_device_channel where stream_proxy_id is not null and id = wdc.id )  where stream_proxy_id is not null;
 
 alter table wvp_device_channel drop device_db_id;
 alter table wvp_device_channel drop stream_push_id;

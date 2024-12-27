115b41e001a3d69e92a097102b9098de79f54f08
648540858 <648540858@qq.com>
2024-12-27 17:53:25 +0800

    [国标PTZ] 添加调用其他wvp的云台控制

--- src/main/java/com/genersoft/iot/vmp/gb28181/service/impl/PTZServiceImpl.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/service/impl/PTZServiceImpl.java
@@ -1,14 +1,19 @@
 package com.genersoft.iot.vmp.gb28181.service.impl;
 
+import com.genersoft.iot.vmp.conf.UserSetting;
 import com.genersoft.iot.vmp.conf.exception.ControllerException;
 import com.genersoft.iot.vmp.gb28181.bean.Device;
+import com.genersoft.iot.vmp.gb28181.bean.DeviceChannel;
 import com.genersoft.iot.vmp.gb28181.bean.Preset;
+import com.genersoft.iot.vmp.gb28181.service.IDeviceChannelService;
 import com.genersoft.iot.vmp.gb28181.service.IPTZService;
 import com.genersoft.iot.vmp.gb28181.transmit.cmd.impl.SIPCommander;
+import com.genersoft.iot.vmp.service.redisMsg.IRedisRpcPlayService;
 import com.genersoft.iot.vmp.vmanager.bean.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import javax.sip.InvalidArgumentException;
 import javax.sip.SipException;
@@ -24,6 +29,15 @@ public class PTZServiceImpl implements IPTZService {
     @Autowired
     private SIPCommander cmder;
 
+    @Autowired
+    private UserSetting userSetting;
+
+    @Autowired
+    private IRedisRpcPlayService redisRpcPlayService;
+
+    @Autowired
+    private IDeviceChannelService deviceChannelService;
+
 
     @Override
     public void ptz(Device device, String channelId, int cmdCode, int horizonSpeed, int verticalSpeed, int zoomSpeed) {
@@ -37,6 +51,14 @@ public class PTZServiceImpl implements IPTZService {
 
     @Override
     public void frontEndCommand(Device device, String channelId, int cmdCode, int parameter1, int parameter2, int combindCode2) {
+        // 判断设备是否属于当前平台, 如果不属于则发起自动调用
+        if (!userSetting.getServerId().equals(device.getServerId())) {
+            // 通道ID
+            DeviceChannel deviceChannel = deviceChannelService.getOneForSource(device.getDeviceId(), channelId);
+            Assert.notNull(deviceChannel, "通道不存在");
+            redisRpcPlayService.frontEndCommand(deviceChannel.getId(), cmdCode, parameter1, parameter2, combindCode2);
+            return;
+        }
         try {
             cmder.frontEndCmd(device, channelId, cmdCode, parameter1, parameter2, combindCode2);
         } catch (SipException | InvalidArgumentException | ParseException e) {

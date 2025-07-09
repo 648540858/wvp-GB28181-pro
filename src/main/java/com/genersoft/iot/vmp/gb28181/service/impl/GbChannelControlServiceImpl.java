3d9e718f266dbe461135f1a409d557181ca15180
lin <648540858@qq.com>
2025-07-09 14:58:26 +0800

    临时提交

--- src/main/java/com/genersoft/iot/vmp/gb28181/service/impl/GbChannelControlServiceImpl.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/service/impl/GbChannelControlServiceImpl.java
@@ -1,10 +1,11 @@
 package com.genersoft.iot.vmp.gb28181.service.impl;
 
-import com.genersoft.iot.vmp.common.enums.ChannelDataType;
 import com.genersoft.iot.vmp.gb28181.bean.*;
+import com.genersoft.iot.vmp.gb28181.service.IDeviceChannelService;
 import com.genersoft.iot.vmp.gb28181.service.IGbChannelControlService;
 import com.genersoft.iot.vmp.service.bean.ErrorCallback;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,13 +15,16 @@ import java.util.List;
 public class GbChannelControlServiceImpl implements IGbChannelControlService {
 
 
+    @Autowired
+    private IDeviceChannelService deviceChannelService;
+
 
     @Override
     public void ptz(CommonGBChannel channel, FrontEndControlCodeForPTZ frontEndControlCode, ErrorCallback<String> callback) {
-        log.info("[通用通道] 云台控制， 通道： {}", channel.getGbDeviceId());
-        if (channel.getDataType() == ChannelDataType.GB28181.value) {
-
-        }else if()
+//        log.info("[通用通道] 云台控制， 通道： {}", channel.getGbDeviceId());
+//        if (channel.getDataType() == ChannelDataType.GB28181.value) {
+//
+//        }else if()
     }
 
     @Override

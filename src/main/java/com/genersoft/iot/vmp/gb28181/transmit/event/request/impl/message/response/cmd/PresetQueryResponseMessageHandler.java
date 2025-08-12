93724bac98fd510538d9fcce53bbcf9db544832b
lin <648540858@qq.com>
2025-08-12 12:28:51 +0800

    修复预置位查询空指针

--- src/main/java/com/genersoft/iot/vmp/gb28181/transmit/event/request/impl/message/response/cmd/PresetQueryResponseMessageHandler.java
+++ src/main/java/com/genersoft/iot/vmp/gb28181/transmit/event/request/impl/message/response/cmd/PresetQueryResponseMessageHandler.java
@@ -81,8 +81,6 @@ public class PresetQueryResponseMessageHandler extends SIPRequestProcessorParent
                 }
                 return;
             }
-            Element sumNumElement = rootElement.element("SumNum");
-            int sumNum = Integer.parseInt(sumNumElement.getText());
             int num = Integer.parseInt(presetListNumElement.attributeValue("Num"));
             List<Preset> presetQuerySipReqList = new ArrayList<>();
             if (num > 0) {
@@ -103,14 +101,8 @@ public class PresetQueryResponseMessageHandler extends SIPRequestProcessorParent
                     presetQuerySipReqList.add(presetQuerySipReq);
                 }
             }
-//            if (presetQuerySipReqList.size() == sumNum) {
-//                responseMessageHandler.handMessageEvent(rootElement, presetQuerySipReqList);
-//            }else {
-//                String sn = getText(element, "SN");
-//                addCatch(cmdType + "_" + sn, rootElement, presetQuerySipReqList);
-//            }
             String sn = getText(element, "SN");
-            addCatch(cmdType + "_" + sn, sumNum,  rootElement, presetQuerySipReqList);
+            addCatch(cmdType + "_" + sn, num,  rootElement, presetQuerySipReqList);
             try {
                 responseAck(request, Response.OK);
             } catch (InvalidArgumentException | ParseException | SipException e) {
